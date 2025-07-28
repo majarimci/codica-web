0b8669a7f5e7b9b749e0805a193b1b17e2bf22d6
Mohammad Hamidi <majari.mci@gmail.com>
2025-07-28 12:59:39 +0330

    .

---
+++ ServiceWorker.js
@@ -0,0 +1,35 @@
+const cacheName = "Academy_Hamrah-Codica-1.1.2";
+const contentToCache = [
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/1af364aed65fdda5a2b5c84fbd206b04.framework.js",
+    "Build/c0eb39694d0f41f386aec609f7220656.data",
+    "Build/7336b22d8b1dbbc45826e817fa29072a.wasm",
+    "TemplateData/style.css",
+    // Add the new loading screen images
+    "TemplateData/tiger-character.png",
+    "TemplateData/lightbulb.png"
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
